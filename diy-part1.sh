13726e946515cfbb29399b5126b864f1068cc712
rgmwxf <81454358+rgmwxf@users.noreply.github.com>
2021-04-26 21:24:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/KFERMercer/openwrt-baidupcs-web.git
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
